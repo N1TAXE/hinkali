fccce3fc1d78fb5d99fb38c7d0b02a1c551a25d6
N1TAXE <n1t4x3@gmail.com>
2023-08-01 11:10:43 +0200

    AppRouter Init

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import {
+    MAIN_ROUTE,
+} from "./utils/consts";
+import MainPage from "./pages/MainPage";
+
+export const publicRoutes = [
+    {
+        path: MAIN_ROUTE,
+        Component: MainPage
+    },
+]
