35220e3838c1ca72c97e844cada3b7434c3977bc
jfoursket <jfoursket@gmail.com>
2023-08-07 20:25:27 +0300

    Main page

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,18 @@
 import {
+    CATALOGUE_ROUTE,
     MAIN_ROUTE,
 } from "./utils/consts";
 import MainPage from "./pages/MainPage";
+import Catalogue from "./pages/Catalogue";
 
 export const publicRoutes = [
     {
         path: MAIN_ROUTE,
         Component: MainPage
+    },
+
+    {
+        path: CATALOGUE_ROUTE,
+        Component: Catalogue
     },
 ]
